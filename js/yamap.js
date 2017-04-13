dc1d071da23308efa2d12ee93e233661840b2f5c
Kirill <elpuffru@gmail.com>
2017-04-13 12:33:09 +0300

    Set map

---
+++ js/yamap.js
@@ -0,0 +1,18 @@
+ymaps.ready(init);
+
+var myMap;
+
+function init() {
+    myMap = new ymaps.Map('map', {
+        center: [59.90906858801045,30.3246718558197],
+        zoom: 15,
+        type: 'yandex#map',
+        controls: []
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+    myPlacemark = new ymaps.Placemark([59.9075005641861,30.324725500000017], {
+        iconColor: '#0095b6'
+    });
+    myMap.geoObjects.add(myPlacemark);
+}
