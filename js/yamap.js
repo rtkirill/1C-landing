4be19d92640a26fd8346eadfd3b4247663014c9d
Kirill <elpuffru@gmail.com>
2017-04-14 11:54:46 +0300

    release

--- js/yamap.js
+++ js/yamap.js
@@ -4,14 +4,14 @@ var myMap;
 
 function init() {
     myMap = new ymaps.Map('map', {
-        center: [59.90906858801045,30.3246718558197],
+        center: [59.90906858801045, 30.3246718558197],
         zoom: 15,
         type: 'yandex#map',
         controls: []
     }, {
         searchControlProvider: 'yandex#search'
     });
-    myPlacemark = new ymaps.Placemark([59.9075005641861,30.324725500000017], {
+    myPlacemark = new ymaps.Placemark([59.9075005641861, 30.324725500000017], {
         iconColor: '#0095b6'
     });
     myMap.geoObjects.add(myPlacemark);
